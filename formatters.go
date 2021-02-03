ca5c3cc9ab5fbac1c3a46840fc3ec51686b94d8a
bjbigler <writebrent@gmail.com>
2021-02-03 13:48:21 -0800

    first commit

---
+++ formatters.go
@@ -0,0 +1,408 @@
+package render
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"strings"
+	"time"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+
+	"cloud.google.com/go/datastore"
+	"github.com/bjbigler/utils"
+	"github.com/shopspring/decimal"
+)
+
+//Location returns New York location
+func location() *time.Location {
+
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		return time.UTC
+	}
+
+	return loc
+}
+
+//FullDateTimeET returns Eastern Time representation (2006-01-02 15:04)
+func FullDateTimeET(val time.Time) string {
+	return val.In(location()).Format("2006-01-02 15:04")
+}
+
+//ShortDateTime returns Eastern Time representation (Jan 01 15:04)
+func ShortDateTime(val time.Time) string {
+	return val.In(location()).Format("Jan 02 3:04pm")
+}
+
+//DateTimeFormal (January 2, 2006 at 3:04PM)
+func DateTimeFormal(val time.Time) string {
+	return val.In(location()).Format("January 02, 2006 at 3:04pm")
+}
+
+//TimeFormat returns AM/PM time format
+func TimeFormat(val time.Time) string {
+	return val.In(location()).Format("3:04pm")
+}
+
+//FullDisplayDate in NY
+func FullDisplayDate(val time.Time) string {
+	return FullDateFormat(val, location())
+}
+
+//FullDateFormat ...
+func FullDateFormat(val time.Time, location *time.Location) string {
+	return val.In(location).Format("Monday, January 2, 2006")
+}
+
+//TimeFormatAmPm ...
+func TimeFormatAmPm(val time.Time, location *time.Location) string {
+	return val.In(location).Format("3:04pm")
+}
+
+//FormatDate ...
+func FormatDate(val time.Time, location *time.Location, format string) string {
+	//fmt.Println(val, location, format)
+	return val.In(location).Format(format)
+}
+
+//FormatDateUTC ...
+func FormatDateUTC(val time.Time, format string) string {
+	return val.In(time.UTC).Format(format)
+}
+
+//DisplayDate (01/02/2006)
+func DisplayDate(val time.Time) string {
+	minTime := time.Time{}
+
+	if val.After(minTime) {
+		return val.In(location()).Format("01/02/2006")
+	}
+	return ""
+}
+
+//DisplayDateTime (01/02/2006 03:04PM)
+func DisplayDateTime(val time.Time) string {
+	minTime := time.Time{}
+
+	if val.After(minTime) {
+		return val.In(location()).Format("01/02/2006 03:04PM")
+	}
+	return ""
+
+}
+
+//DateFormatDisplay (January 2006)
+func DateFormatDisplay(val time.Time) string {
+
+	return val.In(location()).Format("January 2006")
+}
+
+//DateMonth (Jan)
+func DateMonth(val time.Time) string {
+	return val.In(location()).Format("Jan")
+}
+
+//DateDay (2)
+func DateDay(val time.Time) string {
+	return val.In(location()).Format("2")
+}
+
+//DateYear (2006)
+func DateYear(val time.Time) string {
+	return val.In(location()).Format("2006")
+}
+
+//IntlDateDisplay (2006-01-02)
+func IntlDateDisplay(val time.Time) string {
+	return val.In(location()).Format("2006-01-02")
+}
+
+//WhenCompletedDisplay ...
+func WhenCompletedDisplay(val time.Time) string {
+	if val.IsZero() {
+		return ""
+	}
+
+	return "completed " + DateFormatDisplay(val)
+}
+
+//WhenRevisedDisplay ...
+func WhenRevisedDisplay(val time.Time) string {
+	if val.IsZero() {
+		return ""
+	}
+
+	return ", revised " + DateFormatDisplay(val)
+}
+
+//IssueDateFormatDisplay ...
+func IssueDateFormatDisplay(val time.Time) string {
+	if val.IsZero() {
+		return ""
+	}
+	return DateFormatDisplay(val)
+}
+
+//Marshal ...
+func Marshal(v interface{}) template.JS {
+	a, _ := json.Marshal(v)
+	return template.JS(a)
+}
+
+//URLSafeKey returns URL-safe representation of key.
+func URLSafeKey(datastoreKey *datastore.Key) string {
+	if datastoreKey == nil {
+		return ""
+	}
+
+	return datastoreKey.Encode()
+}
+
+//KeyToStringID ...
+func KeyToStringID(datastoreKey *datastore.Key) string {
+	if datastoreKey == nil {
+		return ""
+	}
+
+	return datastoreKey.String()
+}
+
+//Format2 returns a number with two decimal points
+func Format2(d float64) string {
+	return fmt.Sprintf("%.2f", d)
+}
+
+//FormatPhone ...
+func FormatPhone(n string) string {
+	if len(n) == 10 {
+		return "(" + n[0:3] + ") " + n[3:6] + "-" + n[6:10]
+	}
+
+	return n
+}
+
+//Int64Display2FromPrecision10 ...
+func Int64Display2FromPrecision10(number int64) string {
+	//000012357000000 => 1.2347000000
+	dec := decimal.New(number, -10)
+	str := dec.StringFixed(2)
+	return utils.FormatCommas(str)
+}
+
+//Int64Display0 formats an integer with precision 4 to 0 decimals
+func Int64Display0(number int64) string {
+	dec := decimal.New(number, -4)
+	str := dec.StringFixed(0)
+	return utils.FormatCommas(str)
+}
+
+//IntDisplay0 formats an integer with precision 0
+func IntDisplay0(number int) string {
+
+	return utils.RenderInteger("#,###.", number)
+
+}
+
+//Int64Display2 formats an integer with precision 4 to 2 decimals
+func Int64Display2(number int64) string {
+	dec := decimal.New(number, -4)
+	str := dec.StringFixed(2)
+	return utils.FormatCommas(str)
+}
+
+//Int64Display3 formats an integer with precision 4 to 3 decimals
+func Int64Display3(number int64) string {
+	dec := decimal.New(number, -4)
+	str := dec.StringFixed(3)
+	return utils.FormatCommas(str)
+
+}
+
+//Float64Display0 formats a float64 with precision 4 to 3 decimals
+func Float64Display0(number float64) string {
+	number = number / 10000
+	p := message.NewPrinter(language.English)
+	return p.Sprintf("%.0f", number)
+}
+
+//Float64Display2 formatsa float64 with precision 4 to 3 decimals
+func Float64Display2(number float64) string {
+	number = number / 10000
+	p := message.NewPrinter(language.English)
+	return p.Sprintf("%.2f", number)
+}
+
+//Float64Display3 formats a float64 with precision 4 to 3 decimals
+func Float64Display3(number float64) string {
+	number = number / 10000
+	p := message.NewPrinter(language.English)
+	return p.Sprintf("%.3f", number)
+}
+
+//DecimalDisplay0 ...
+func DecimalDisplay0(dec decimal.Decimal) string {
+	str := dec.StringFixed(0)
+	return utils.FormatCommas(str)
+}
+
+//DecimalDisplay2 ...
+func DecimalDisplay2(dec decimal.Decimal) string {
+	str := dec.StringFixed(2)
+	return utils.FormatCommas(str)
+}
+
+//DecimalDisplay3 ...
+func DecimalDisplay3(dec decimal.Decimal) string {
+	str := dec.StringFixed(3)
+	return utils.FormatCommas(str)
+}
+
+//PlusOneZeroPad adds one to the value provided and pads with a zero
+func PlusOneZeroPad(val int) string {
+	newVal := val + 1
+	return ZeroPad(newVal)
+}
+
+//PlusOne adds one to the value provided
+func PlusOne(val int) int {
+	return val + 1
+}
+
+//Add ...
+func Add(a, b int) int {
+	return a + b
+}
+
+//Subtract ...
+func Subtract(a, b int) int {
+	return a - b
+}
+
+//Multiply ...
+func Multiply(a, b int) int {
+	return a * b
+}
+
+//Divide ...
+func Divide(a, b int) float64 {
+	return float64(a) / float64(b)
+}
+
+//Dashes returns dashes related to the level,
+//e.g., level 1 = zero dashes, level 2 = two dashes, etc.
+func Dashes(level int) string {
+	level = level - 1
+
+	result := ""
+
+	for i := 0; i < level; i++ {
+		result += "—"
+	}
+
+	return result
+}
+
+//ZeroPad adds a zero in front of the value
+func ZeroPad(val int) string {
+	return fmt.Sprintf("%02d", val)
+}
+
+//ZeroPad64 adds a zero in front of the value
+func ZeroPad64(val int64) string {
+	return fmt.Sprintf("%02d", val)
+}
+
+//FirstInitial returns first initial of name
+func FirstInitial(name string) string {
+	return name[0:1]
+}
+
+//CalcTabIndex ...
+func CalcTabIndex(index int, num int, base int) int {
+	return (index * base) + num
+}
+
+//IsToday ...
+func IsToday(dte time.Time) bool {
+
+	yearNow, monthNow, dayNow := time.Now().In(location()).Date()
+	yearDte, monthDte, dayDte := dte.In(location()).Date()
+
+	if yearNow == yearDte && monthNow == monthDte && dayNow == dayDte {
+		return true
+	}
+
+	return false
+}
+
+//NewLineToBR ...
+func NewLineToBR(s string) template.HTML {
+	return template.HTML(strings.Replace(s, "\n", "<br />", -1))
+}
+
+//DictHelper ...
+func DictHelper(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+
+	dict := make(map[string]interface{}, len(values)/2)
+
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+
+		dict[key] = values[i+1]
+	}
+	return dict, nil
+}
+
+//HTMLEscape ...
+func HTMLEscape(input string) template.HTML {
+	return template.HTML(input)
+}
+
+//ToUppercase ...
+func ToUppercase(v string) string {
+	return strings.ToUpper(v)
+}
+
+//ToLowercase ...
+func ToLowercase(v string) string {
+	return strings.ToLower(v)
+}
+
+//ToTitleCase ...
+func ToTitleCase(v string) string {
+	return strings.Title(v)
+}
+
+//PrepPhone replaces ")", "(", "-", " "
+//to make sure it will show up in a "tel"
+//tag with proper format.
+func PrepPhone(v string) string {
+
+	v = strings.Replace(v, "(", "", -1)
+	v = strings.Replace(v, ")", "", -1)
+	v = strings.Replace(v, " ", "", -1)
+	v = strings.Replace(v, "-", "", -1)
+	v = strings.Replace(v, ",", "", -1)
+
+	return v
+}
+
+//Int64ToTime returns an HTML-time-formatted
+//string from an int64. Example: 835 -> 08:35
+func Int64ToTime(v int64) string {
+
+	hours := fmt.Sprintf("%02d", v/100)
+	minutes := fmt.Sprintf("%02d", v%100)
+	return fmt.Sprintf("%s:%s", hours, minutes)
+
+}
---
+++ render.go
@@ -0,0 +1,538 @@
+package render
+
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bjbigler/utils"
+	"golang.org/x/net/context"
+)
+
+//ParseTemplateSets parses sets of files into templates, one per page needed.
+//The *baseTemplate* should be a relative path, e.g., "views/master.html"
+//In *sets*, the first string should be the template map lookup name.
+//Ex: {"authenticators", "views/master.html", "views/authenticators.html"}
+func ParseTemplateSets(baseTemplate string, sets [][]string) (templates map[string]*template.Template, err error) {
+
+	templates = make(map[string]*template.Template)
+
+	funcMap := GetFuncMap()
+
+	templateName := baseTemplate
+	templateParts := strings.Split(baseTemplate, "/")
+	if len(templateParts) > 0 {
+		templateName = templateParts[len(templateParts)-1]
+	}
+
+	for _, set := range sets {
+
+		t := template.New(templateName).Funcs(funcMap)
+
+		templateSet := []string{baseTemplate}
+		templateSet = append(templateSet, set[1:]...)
+
+		_, err = t.ParseFiles(templateSet...)
+
+		if err != nil {
+			return nil, err
+		}
+
+		//put the template in map using the lookup name
+		lookupName := set[0]
+		templates[lookupName] = t
+
+	}
+
+	return templates, err
+}
+
+//Template renders template from the template map produced by ParseTemplateSets
+func Template(w http.ResponseWriter, templates map[string]*template.Template, model interface{}, templateIndex string) error {
+
+	template, ok := templates[templateIndex]
+	if ok {
+		return template.Execute(w, &model)
+	}
+	return fmt.Errorf("template %s not found", templateIndex)
+
+}
+
+//FindAndParseTemplates parses all templates
+func FindAndParseTemplates() (*template.Template, error) {
+	funcMap := GetFuncMap()
+
+	cleanRoot := filepath.Clean("views")
+	pfx := len(cleanRoot) + 1
+	root := template.New("")
+
+	err := filepath.Walk(cleanRoot, func(path string, info os.FileInfo, e1 error) error {
+		if !info.IsDir() && strings.HasSuffix(path, ".html") {
+			if e1 != nil {
+				return e1
+			}
+
+			b, e2 := ioutil.ReadFile(path)
+			if e2 != nil {
+				return e2
+			}
+
+			name := path[pfx:]
+			t := root.New(name).Funcs(funcMap)
+			_, e2 = t.ParseFiles(string(b))
+			if e2 != nil {
+				return e2
+			}
+		}
+
+		return nil
+	})
+
+	return root, err
+}
+
+//ToBrowser pairs master.html template to whatever instanceTemplate gets
+//passed in.
+func ToBrowser(w http.ResponseWriter, model interface{}, templates ...string) error {
+
+	templates = append([]string{"views/master.html"}, templates...)
+
+	funcMap := GetFuncMap()                       //Sets up the funcMaps to be used on templates, e.g., formatters like displayDate
+	tmpl := template.New("master.html")           //Initializes named template
+	funcs := tmpl.Funcs(funcMap)                  //associates the funcMap with the template
+	parsed, err := funcs.ParseFiles(templates...) //Parses the templates
+	t := template.Must(parsed, err)               //Creates template
+	err = t.Execute(w, model)                     //Merges template with data
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+//RedirectTo ...
+func RedirectTo(w http.ResponseWriter, redirectURL *url.URL) {
+	jsonOut := fmt.Sprintf(`{"statusCode": %v, "redirectTo":"%s" }`, 6, redirectURL.String())
+	JSONToBrowser(w, []byte(jsonOut))
+}
+
+//ToBrowserNoMaster prints out template with no master.
+func ToBrowserNoMaster(w http.ResponseWriter, instanceTemplate string, model interface{}) {
+
+	funcMap := GetFuncMap()
+
+	//Here, we grab the name by grabbing the text after the final slash
+	positionOfLastSlash := strings.LastIndex(instanceTemplate, "/")
+	templateName := string(instanceTemplate[positionOfLastSlash+1:])
+	t := template.Must(template.New(templateName).Funcs(funcMap).ParseFiles(instanceTemplate))
+
+	if err := t.Execute(w, model); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+//ToBrowserNoMasterNew ...
+func ToBrowserNoMasterNew(w http.ResponseWriter, model interface{}, templates ...string) error {
+	if len(templates) == 0 {
+		return fmt.Errorf("no templates supplied")
+	}
+
+	funcMap := GetFuncMap()
+
+	//Here, we grab the name by grabbing the text after the final slash
+	positionOfLastSlash := strings.LastIndex(templates[0], "/")
+	templateName := string(templates[0][positionOfLastSlash+1:])
+	t := template.Must(template.New(templateName).Funcs(funcMap).ParseFiles(templates...))
+
+	if err := t.Execute(w, model); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	return nil
+}
+
+//ToString renders the instanceTemplate alone, without any master supplied.
+//It's used for html fragments, largely in AJAX.
+func ToString(model interface{}, templates ...string) (string, error) {
+
+	if len(templates) == 0 {
+		return "", fmt.Errorf("no template(s) specified")
+	}
+
+	funcMap := GetFuncMap()
+
+	instanceTemplate := templates[0]
+	positionOfLastSlash := strings.LastIndex(instanceTemplate, "/")
+	templateName := string(instanceTemplate[positionOfLastSlash+1:])
+
+	tmpl := template.New(templateName)            //Initializes named template
+	funcs := tmpl.Funcs(funcMap)                  //associates the funcMap with the template
+	parsed, err := funcs.ParseFiles(templates...) //Parses the templates
+	t := template.Must(parsed, err)               //Creates template
+
+	var doc bytes.Buffer
+
+	err = t.Execute(&doc, model)
+	if err != nil {
+		return "", err
+	}
+
+	return doc.String(), nil
+}
+
+//ToStringFromString renders a string HTML template
+func ToStringFromString(html string, model interface{}) (result string, err error) {
+	funcMap := GetFuncMap()                  //Sets up the funcMaps to be used on templates, e.g., formatters like displayDate
+	bodyTemplate := template.New("template") //Initializes named template
+	funcs := bodyTemplate.Funcs(funcMap)     //associates the funcMap with the template
+
+	parsed, err := funcs.Parse(html) //Parses the template
+
+	if err != nil {
+		return "", err
+	}
+
+	t := template.Must(parsed, err) //Creates template
+	if err != nil {
+		return "", err
+	}
+
+	var b bytes.Buffer
+
+	err = t.Execute(&b, model) //Executes
+	if err != nil {
+		return "", err
+	}
+
+	return b.String(), err
+}
+
+//ToHTMLOld renders a template as template.HTML to use as html fragments when compositing
+//a page together.
+func ToHTMLOld(instanceTemplate string, model interface{}) (template.HTML, error) {
+
+	funcMap := GetFuncMap()
+
+	//For some reason, the template name has to be filename, e.g., template.html.
+	//The templates are supplied to the function in terms of their folder location,
+	//e.g., /views/research/template.html.
+	//Here, we grab the name by grabbing the text after the final slash
+	positionOfLastSlash := strings.LastIndex(instanceTemplate, "/")
+	templateName := string(instanceTemplate[positionOfLastSlash+1:])
+
+	t := template.Must(template.New(templateName).Funcs(funcMap).ParseFiles(instanceTemplate))
+
+	var doc bytes.Buffer
+
+	err := t.Execute(&doc, model)
+	if err != nil {
+		return template.HTML(""), err
+	}
+
+	return template.HTML(doc.String()), nil
+}
+
+//ToHTML renders a template as template.HTML to use as html fragments when compositing
+//a page together.
+func ToHTML(model interface{}, templates ...string) (template.HTML, error) {
+
+	if len(templates) == 0 {
+		return template.HTML(""), fmt.Errorf("no templates passed into ToHTML()")
+	}
+	funcMap := GetFuncMap()
+
+	//For some reason, the template name has to be filename, e.g., template.html.
+	//The templates are supplied to the function in terms of their folder location,
+	//e.g., /views/research/template.html.
+	//Here, we grab the name by grabbing the text after the final slash
+
+	positionOfLastSlash := strings.LastIndex(templates[0], "/")
+	templateName := strings.TrimSpace(string(templates[0][positionOfLastSlash+1:]))
+
+	tmpl := template.New(templateName) //Initializes named template
+
+	funcs := tmpl.Funcs(funcMap) //associates the funcMap with the template
+
+	parsed, err := funcs.ParseFiles(templates...) //Parses templates
+
+	if err != nil {
+		return template.HTML(""), err
+	}
+
+	t := template.Must(parsed, err) //Creates template
+
+	var doc bytes.Buffer
+
+	err = t.Execute(&doc, model)
+	if err != nil {
+		return template.HTML(""), err
+	}
+
+	return template.HTML(doc.String()), nil
+}
+
+//JSONToBrowser sends a JSON []byte to the browser
+func JSONToBrowser(w http.ResponseWriter, json []byte) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	_, err := w.Write(json)
+
+	return err
+}
+
+//StringToBrowser takes *html* string and sends it to the browser.
+func StringToBrowser(w http.ResponseWriter, html string) error {
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	_, err := w.Write([]byte(html))
+
+	return err
+}
+
+//CsvToBrowser takes a [][]string and sends it to the browser as a CSV file.
+func CsvToBrowser(w http.ResponseWriter, csvRecords [][]string, filename string) {
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename="+filename)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := &bytes.Buffer{}
+	csvWriter := csv.NewWriter(b)
+	csvWriter.WriteAll(csvRecords)
+
+	w.Write(b.Bytes())
+}
+
+//WriteXlsToBrowser takes a string formatted as Office XML and outputs it to the browswer as a file.
+func WriteXlsToBrowser(ctx context.Context, w http.ResponseWriter, xls string, filename string) {
+	w.Header().Set("Content-Type", "application/vnd.ms-excel")
+	w.Header().Set("Content-Disposition", "attachment;filename="+filename)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := bytes.NewBuffer([]byte(xls))
+
+	if _, err := b.WriteTo(w); err != nil {
+		output := fmt.Sprintf("%v", err)
+		fmt.Fprint(w, output)
+	}
+}
+
+//WriteIcsToBrowser delivers iCalendar files to the browser
+func WriteIcsToBrowser(w http.ResponseWriter, calendar string, filename string) {
+	w.Header().Set("Content-Type", "text/calendar")
+	w.Header().Set("Content-Disposition", "attachment;filename="+filename)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := bytes.NewBuffer([]byte(calendar))
+
+	if _, err := b.WriteTo(w); err != nil {
+		output := fmt.Sprintf("%v", err)
+		fmt.Fprint(w, output)
+	}
+}
+
+//PDFToBrowser streams PDF file to browser. Its main purpose
+//is security: instead of linking to the file system,
+//code calling this func requires a login.
+func PDFToBrowser(w http.ResponseWriter, path string) error {
+	streamPDFbytes, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return err
+	}
+
+	lastSlash := strings.LastIndex(path, "/")
+	filename := path[lastSlash+1:]
+
+	w.Header().Set("Content-type", "application/pdf")
+	w.Header().Set("Content-Disposition", "inline;filename="+filename)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := bytes.NewBuffer(streamPDFbytes)
+	if _, err := b.WriteTo(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//PDFBytesToBrowser streams PDF file to browser. Its main purpose
+//is security: instead of linking to the file system,
+//code calling this func requires a login.
+func PDFBytesToBrowser(w http.ResponseWriter, fileName string, file []byte) error {
+
+	w.Header().Set("Content-type", "application/pdf")
+	w.Header().Set("Content-Disposition", "inline;filename="+fileName)
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := bytes.NewBuffer(file)
+	if _, err := b.WriteTo(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//GenericBytesToBrowser streams a file without setting its content type
+func GenericBytesToBrowser(w http.ResponseWriter, fileName string, file []byte) error {
+
+	// w.Header().Set("Content-type", "application/pdf")
+	// w.Header().Set("Content-Disposition", "inline;filename="+fileName)
+	// w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	b := bytes.NewBuffer(file)
+	if _, err := b.WriteTo(w); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//ReportError sends JSON message with "statusCode:0" and "error:" with the error specified
+func ReportError(w http.ResponseWriter, message interface{}) {
+	type helper struct {
+		StatusCode int    `json:"statusCode"`
+		Error      string `json:"error"`
+	}
+
+	h := helper{
+		StatusCode: 0,
+		Error:      fmt.Sprintf("%v", message),
+	}
+
+	jsonOut, _ := json.Marshal(h)
+	JSONToBrowser(w, jsonOut)
+}
+
+//ReportMessage sends JSON messagae with "statusCode:0" and "msg:" *message
+func ReportMessage(w http.ResponseWriter, message string) {
+
+	type helper struct {
+		StatusCode int    `json:"statusCode"`
+		Message    string `json:"msg"`
+	}
+
+	h := helper{
+		StatusCode: 0,
+		Message:    message,
+	}
+
+	jsonOut, _ := json.Marshal(h)
+	JSONToBrowser(w, jsonOut)
+
+}
+
+//ReportRedirect sends JSON message with "statusCode" of 1 "redirect" equal to the *redirect*
+//provided. If  *redirectID* is also provided, the javascript will
+//attempt to scroll into view any found element with that ID.
+func ReportRedirect(w http.ResponseWriter, redirect string) {
+	type helper struct {
+		StatusCode int    `json:"statusCode"`
+		Redirect   string `json:"redirect"`
+	}
+
+	h := helper{
+		StatusCode: 1,
+		Redirect:   redirect,
+	}
+
+	jsonOut, _ := json.Marshal(h)
+	JSONToBrowser(w, jsonOut)
+}
+
+//ReportErrors sends JSON message with "statusCode:0" and the errors specified
+func ReportErrors(w http.ResponseWriter, errors []error) {
+	errorsJSON, _ := json.Marshal(errors)
+	jsonOut := fmt.Sprintf(`{"statusCode": %v, "errors":%v }`, 0, string(errorsJSON))
+	JSONToBrowser(w, []byte(jsonOut))
+}
+
+//ReportSuccess sends JSON message with "statusCode:1"
+func ReportSuccess(w http.ResponseWriter) {
+	jsonOut := fmt.Sprintf(`{"statusCode": %v }`, 1)
+	JSONToBrowser(w, []byte(jsonOut))
+}
+
+//ReportReload sends JSON message with "statusCode:5", which doGetFetch/doPostFetch
+//interpret as a reload
+func ReportReload(w http.ResponseWriter) {
+	jsonOut := fmt.Sprintf(`{"statusCode": %v }`, 5)
+	JSONToBrowser(w, []byte(jsonOut))
+}
+
+//GetFuncMap provides a set of utility functions to help format data on an HTML output page.
+func GetFuncMap() map[string]interface{} {
+	return template.FuncMap{
+		"formatDate":                   FormatDate,
+		"formatDateUTC":                FormatDateUTC,
+		"displayDate":                  DisplayDate,
+		"displayDateTime":              DisplayDateTime,
+		"dateFormatDisplay":            DateFormatDisplay,
+		"dateMonth":                    DateMonth,
+		"dateDay":                      DateDay,
+		"dateYear":                     DateYear,
+		"dateTimeFormal":               DateTimeFormal,
+		"shortDateTime":                ShortDateTime,
+		"decimalDisplay0":              DecimalDisplay0, //Precision 6
+		"decimalDisplay2":              DecimalDisplay2, //Precision 6
+		"decimalDisplay3":              DecimalDisplay3, //Precision 6
+		"intDisplay0":                  IntDisplay0,
+		"int64Display0":                Int64Display0,                //Precision 4
+		"int64Display2":                Int64Display2,                //Precision 4
+		"int64Display3":                Int64Display3,                //Precision 4
+		"float64Display0":              Float64Display0,              //Precision 4
+		"float64Display2":              Float64Display2,              //Precision 4
+		"float64Display3":              Float64Display3,              //Precision 4
+		"int64Display2FromPrecision10": Int64Display2FromPrecision10, //Precision 10
+		"fullDateTimeET":               FullDateTimeET,               //
+		"whenCompletedDisplay":         WhenCompletedDisplay,         //
+		"whenRevisedDisplay":           WhenRevisedDisplay,           //
+		"issueDateFormatDisplay":       IssueDateFormatDisplay,       //
+		"marshal":                      Marshal,                      //
+		"urlSafeKey":                   URLSafeKey,                   //
+		"keyToStringID":                KeyToStringID,                //
+		"format2":                      Format2,                      //
+		"formatPhone":                  FormatPhone,                  //
+		"plusOne":                      PlusOne,                      //
+		"add":                          Add,                          //Add two numbers
+		"subtract":                     Subtract,                     //Subtract two numbers
+		"multiply":                     Multiply,
+		"divide":                       Divide,
+		"plusOneZeroPad":               PlusOneZeroPad, //
+		"zeroPad":                      ZeroPad,        //
+		"zeroPad64":                    ZeroPad64,      //
+		"dashes":                       Dashes,         //
+		"fullDisplayDate":              FullDisplayDate,
+		"fullDateFormat":               FullDateFormat, //
+		"timeFormatAmPm":               TimeFormatAmPm,
+		"intlDateDisplay":              IntlDateDisplay, //
+		"firstInitial":                 FirstInitial,    //
+		"calcTabIndex":                 CalcTabIndex,    //
+		"isToday":                      IsToday,         //
+		"newLineToBR":                  NewLineToBR,     //
+		"timeFormat":                   TimeFormat,      //
+		"dict":                         DictHelper,      //
+		"htmlEscape":                   HTMLEscape,      //
+		"toUppercase":                  ToUppercase,     //
+		"toLowercase":                  ToLowercase,     //
+		"toTitleCase":                  ToTitleCase,
+		"int64ToTime":                  Int64ToTime, //Converts, e.g., 835 to 8:35
+		"academicYearView":             utils.AcademicYearView,
+		"prepPhone":                    PrepPhone, //Preps a phone number for use in an HTML tel tag
+		"safe": func(s string) template.HTML {
+			return template.HTML(s)
+		},
+	}
+}
